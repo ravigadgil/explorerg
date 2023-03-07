ea1d34679e1c1fecae3bc297ccf0702cdf9bee41
Chetan1330 <41874307+Chetan1330@users.noreply.github.com>
2023-03-08 01:37:29 +0530

    Delete gunicorn-cfg.py

--- gunicorn-cfg.py
+++
@@ -1,8 +0,0 @@
-wsgi_app = "LearningPortal.wsgi:application"
-bind = '0.0.0.0:8000'
-workers = 1
-accesslog = '-'
-reload = True
-loglevel = 'debug'
-capture_output = True
-enable_stdio_inheritance = True
